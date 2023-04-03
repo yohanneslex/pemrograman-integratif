723f60695878a3fa89f286cce6decf6b0e9f1e08
Yohanneslex <50076171+yohanneslex@users.noreply.github.com>
2023-04-03 09:58:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,97 @@
-// Import package 
-const grpc = require('@grpc/grpc-js');
-var protoLoader = require('@grpc/proto-loader');
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const mysql = require('mysql2');
 
-// Define Proto path 
-const PROTO_PATH = './mahasiswa.proto';
+const PROTO_PATH = './grpc.proto';
 
-const options = {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);
+const userProto = grpc.loadPackageDefinition(packageDefinition).User;
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'grpcfix',
+  waitForConnections: true,
+  connectionLimit: 10,
+});
+
+function getUser(call, callback) {
+  const userId = call.request.userId;
+  pool.query('SELECT * FROM users WHERE user_id = ?', [userId], (error, results) => {
+    if (error) {
+      console.error(error);
+      callback({ code: grpc.status.INTERNAL, message: 'Internal Server Error' });
+      return;
+    }
+    if (results.length === 0) {
+      callback({ code: grpc.status.NOT_FOUND, message: 'User not found' });
+      return;
+    }
+    const userData = results[0];
+    const user = {
+      userId: userData.user_id,
+      name: userData.name,
+      age: userData.age,
+    };
+    callback(null, user);
+  });
 }
 
-var packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
-
-// Load Proto 
-const mahasiswaProto = grpc.loadPackageDefinition(packageDefinition);
-
-const server = new grpc.Server();
-
-// Dummy data 
-const mahasiswa = {
-    mahasiswa: [
-        {
-            id: "1",
-            nama: "Rudi",
-            nrp: "5119",
-            nilai: 59
-          },
-          {
-            id: "2",
-            nama: "Budi",
-            nrp: "5118",
-            nilai: 60
-          }
-    ]
+function addUser(call, callback) {
+  const name = call.request.name;
+  const age = call.request.age;
+  pool.query('INSERT INTO users (name, age) VALUES (?, ?)', [name, age], (error, result) => {
+    if (error) {
+      console.error(error);
+      callback({ code: grpc.status.INTERNAL, message: 'Internal Server Error' });
+      return;
+    }
+    const userId = result.insertId;
+    const user = { userId, name, age };
+    callback(null, user);
+  });
 }
 
-server.addService(mahasiswaProto.MahasiswaService.service, {
-  getAll: (_, callback) => {
-    callback(null, mahasiswa);
-  },
-  addMahasiswa: (call, callback) => {
-    const _mahasiswa = {...call.request};
-    mahasiswa.mahasiswa.push(_mahasiswa);
-    callback(null, _mahasiswa);
-  }
-})
+function updateUser(call, callback) {
+  const userId = call.request.userId;
+  const name = call.request.name;
+  const age = call.request.age;
+  pool.query('UPDATE users SET name = ?, age = ? WHERE user_id = ?', [name, age, userId], error => {
+    if (error) {
+      console.error(error);
+      callback({ code: grpc.status.INTERNAL, message: 'Internal Server Error' });
+      return;
+    }
+    const user = { userId, name, age };
+    callback(null, user);
+  });
+}
 
-// Start server 
-server.bindAsync(
-  "127.0.0.1:50051",
-  grpc.ServerCredentials.createInsecure(),
-  (error, port) => {
-    console.log("Server running at http://127.0.0.1:50051");
+function deleteUser(call, callback) {
+  const userId = call.request.userId;
+  pool.query('DELETE FROM users WHERE user_id = ?', [userId], error => {
+    if (error) {
+      console.error(error);
+      callback({ code: grpc.status.INTERNAL, message: 'Internal Server Error' });
+      return;
+    }
+    callback(null, {});
+  });
+}
+
+function main() {
+  const server = new grpc.Server();
+  server.addService(userProto.UserService.service, {
+    getUser,
+    addUser,
+    updateUser,
+    deleteUser,
+  });
+  server.bindAsync('127.0.0.1:5000', grpc.ServerCredentials.createInsecure(), () => {
+    console.log('Server running at http://127.0.0.1:5000');
     server.start();
-  }
-)
+  });
+}
+
+main();
